c9a617f2462b0a09576c357cdb2f1ad73cb71e6c
OscarEscamilla <escamillaluquenoo@gmail.com>
2024-06-03 11:57:44 -0600

    add loading poster images to view cells

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
+enum Sections: Int{
+    case TrendingMovies = 0
+    case TrendingTv =  1
+    case PopularMovies = 2
+    case UpcomingMovies = 3
+    case TopRatedMovies = 4
+}
+
 class HomeViewController: UIViewController {
     
     let sectionTitles: [String] = ["Trending Movies","Trending TV", "Popular", "Upcoming Movies", "Top rated"]
@@ -33,7 +41,7 @@ class HomeViewController: UIViewController {
         let headerView = HeroHeaderUIView(frame:  CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
         
-        getTrendingMovies()
+    
     }
     
     private func configureNavBar(){
@@ -54,16 +62,7 @@ class HomeViewController: UIViewController {
         homeFeedTable.frame =  view.bounds
     }
     
-    private func getTrendingMovies(){
-        APICaller.shared.getTrendingMovies { results in
-            switch results {
-            case .success(let movies):
-                print(movies)
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
+    
     
 }
 
@@ -80,6 +79,65 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        switch indexPath.section {
+            case Sections.TrendingMovies.rawValue:
+            
+                APICaller.shared.getTrendingMovies { result in
+                    switch result {
+                    case .success(let titles):
+                            cell.configure(with: titles)
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                    }
+                }
+            
+        case Sections.TopRatedMovies.rawValue:
+            
+                APICaller.shared.getTopRatedMovies { result in
+                    switch result {
+                        case .success(let titles):
+                                cell.configure(with: titles)
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                    }
+                } 
+        
+        case Sections.TrendingTv.rawValue:
+            
+                APICaller.shared.getTrendingTvs { result in
+                    switch result {
+                        case .success(let titles):
+                                cell.configure(with: titles)
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                    }
+                }
+            
+        case Sections.PopularMovies.rawValue:
+            
+                APICaller.shared.getPopularMovies { result in
+                    switch result {
+                        case .success(let titles):
+                                cell.configure(with: titles)
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                    }
+                }
+            
+        case Sections.UpcomingMovies.rawValue:
+            
+                APICaller.shared.getUpcomingMovies { result in
+                    switch result {
+                        case .success(let titles):
+                                cell.configure(with: titles)
+                        case .failure(let error):
+                            print(error.localizedDescription)
+                    }
+                }
+        default:
             return UITableViewCell()
         }
         
